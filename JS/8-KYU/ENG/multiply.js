2c2f5908ea563544b45531ce53a927dd9826a9bd
Miguel Zafra <mzafram2001@gmail.com>
2024-01-17 21:24:39 +0100

    ADD multiply js

---
+++ JS/8-KYU/ENG/multiply.js
@@ -0,0 +1,13 @@
+// NAME: Multiply
+// AUTHOR: jhoffner
+// LEVEL: 8 Kyu
+//
+// DESCRIPTION:
+// This code does not execute properly. Try to figure out why.
+
+// This is a simple function named 'multiply' that takes two parameters, 'a' and 'b'.
+function multiply(a, b) {
+    // The 'return' keyword is used to send the result of the multiplication back to the caller.
+    // The result is obtained by multiplying the values of 'a' and 'b'.
+    return a * b;
+}
---
+++ JS/8-KYU/ESP/multiply.js
@@ -0,0 +1,13 @@
+// NOMBRE: Multiplicar
+// AUTOR: jhoffner
+// NIVEL: 8 Kyu
+//
+// DESCRIPCIÓN:
+// Este código no se ejecuta correctamente. Intenta averiguar por qué.
+
+// Esta es una función sencilla llamada 'multiply' que toma dos parámetros, 'a' y 'b'.
+function multiply(a, b) {
+    // La palabra clave 'return' se utiliza para enviar el resultado de la multiplicación de vuelta al que llama a la función.
+    // El resultado se obtiene multiplicando los valores de 'a' y 'b'.
+    return a * b;
+}
