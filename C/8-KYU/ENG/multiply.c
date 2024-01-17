20110471c62eb80505d16432fa1184b15bd47381
Miguel Zafra <mzafram2001@gmail.com>
2024-01-17 19:26:19 +0100

    ADD multiply in c

---
+++ C/8-KYU/ENG/multiply.c
@@ -0,0 +1,15 @@
+// NAME: Multiply
+// AUTHOR: jhoffner
+// LEVEL: 8 Kyu
+//
+// DESCRIPTION:
+// This code does not execute properly. Try to figure out why.
+
+// This is a C function that multiplies two integers and returns the result.
+int multiply(int a, int b) {
+  // The function takes two parameters, 'a' and 'b', both of type int.
+
+  // The 'return' statement calculates the product of 'a' and 'b' using the '*' operator
+  // and returns the result to the caller.
+  return a * b;
+}
---
+++ C/8-KYU/ESP/multiply.c
@@ -0,0 +1,15 @@
+// NOMBRE: Multiplicar
+// AUTOR: jhoffner
+// NIVEL: 8 Kyu
+//
+// DESCRIPCIÓN:
+// Este código no se ejecuta correctamente. Intenta averiguar por qué.
+
+// Esta es una función en C que multiplica dos enteros y devuelve el resultado.
+int multiplicar(int a, int b) {
+  // La función toma dos parámetros, 'a' y 'b', ambos de tipo int.
+
+  // La declaración 'return' calcula el producto de 'a' y 'b' usando el operador '*'
+  // y devuelve el resultado al que llama a la función.
+  return a * b;
+}
