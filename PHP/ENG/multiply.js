ae08462d96c1340d87ccc0020b21396ccb41877d
Miguel Zafra <mzafram2001@gmail.com>
2024-01-21 10:51:46 +0100

    ADD multiply in php

---
+++ PHP/ENG/multiply.js
@@ -0,0 +1,14 @@
+// NOMBRE: Multiply
+// AUTOR: jhoffner
+// NIVEL: 8 Kyu
+//
+// DESCRIPCIÓN:
+// This code does not execute properly. Try to figure out why.
+//
+// Define a function named 'multiply' with two parameters: $a and $b
+function multiply($a, $b) {
+    // Calculate the product of $a and $b using the '*' operator
+    $result = $a * $b;
+    // Return the calculated result
+    return $result;
+}
---
+++ PHP/ESP/multiply.js
@@ -0,0 +1,14 @@
+// NOMBRE: Multiply
+// AUTOR: jhoffner
+// NIVEL: 8 Kyu
+//
+// DESCRIPCIÓN:
+// Este código no se ejecuta correctamente. Intenta descubrir por qué.
+//
+// Define una función llamada 'multiply' con dos parámetros: $a y $b
+function multiply($a, $b) {
+    // Calcula el producto de $a y $b usando el operador '*'
+    $result = $a * $b;
+    // Devuelve el resultado calculado
+    return $result;
+}
